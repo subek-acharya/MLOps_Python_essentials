fd927357513aff9d3081b9c35dbe257b8b16a1b8
Subek Acharya <subekacharya@gmail.com>
2025-09-05 21:20:25 -0400

    Simple flask app

---
+++ flask-app/webapp/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def two_hundred():
+    return "<h2>200! all is good with URI</h2>"
+
+@app.route("/error")
+def error():
+    abort(500, "Oops!!! There is some error!")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, host="0.0.0.0")
+
+
